6c11e5b41ca1478427a5593a385e7bc5e8aa5eda
E S <eylon20@gmail.com>
2017-08-11 16:14:07 +0300

    lots of shit

--- models/client.js
+++ models/client.js
@@ -4,19 +4,6 @@ var User = require('./user');
 // Define client as a discriminator of user
 var clientSchema = User.discriminator('client', new mongoose.Schema(
     {
-
-        appointments_paths:
-        [{
-            branch: mongoose.Schema.Types.ObjectId,
-            employee: mongoose.Schema.Types.ObjectId,
-            appointment: mongoose.Schema.Types.ObjectId
-        }],
-        queue_paths:
-        [{
-            branch: mongoose.Schema.Types.ObjectId,
-            queue: mongoose.Schema.Types.ObjectId
-        }],
-
         appointments: [{
             branch_name: String,
             branch: { type: mongoose.Schema.Types.ObjectId, ref: "branch" },
